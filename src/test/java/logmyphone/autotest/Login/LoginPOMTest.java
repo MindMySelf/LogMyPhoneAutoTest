70cd6e5d70d357da67f8a15a3c0fc5916a0347d2
arpdkvcs <arpdkvcs@gmail.com>
2023-12-07 14:59:55 +0100

    Add parameterized test to LoginPOMTest class

--- src/test/java/logmyphone/autotest/Login/LoginPOMTest.java
+++ src/test/java/logmyphone/autotest/Login/LoginPOMTest.java
@@ -1,6 +1,7 @@
 package logmyphone.autotest.Login;
 
-import logmyphone.autotest.Dashboard.DashboardPOM;
+import logmyphone.autotest.dashboard.DashboardPOM;
+import logmyphone.autotest.landingPage.LandingPagePOM;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,15 +9,19 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LoginPOMTest {
     private static final String URL = "localhost:3000";
     private WebDriver driver;
+    private LandingPagePOM landingPage;
     private LoginPOM login;
     private DashboardPOM dashboard;
 
     @BeforeEach
     void setup() {
         driver = new FirefoxDriver();
+        landingPage = new LandingPagePOM(driver);
         login = new LoginPOM(driver);
         dashboard = new DashboardPOM(driver);
         driver.get(URL);
@@ -24,8 +29,17 @@ public class LoginPOMTest {
 
     @ParameterizedTest
     @CsvFileSource(files = "src/test/java/logmyphone/autotest/Login/resources/loginCredentialsAndExpectations.csv", numLinesToSkip = 1)
-    void testLogin() {
+    void testLogin(String email, String password, String dashboardVisible) {
+        landingPage.clickOnLoginButton();
 
+        login.enterEmail(email);
+        login.enterPassword(password);
+        login.clickSignIn();
+
+        boolean expectedResult = dashboardVisible.equals("true");
+        boolean actualResult = dashboard.checkVisibilityOfProfileButton();
+
+        assertEquals(expectedResult, actualResult);
     }
 
     @AfterEach
