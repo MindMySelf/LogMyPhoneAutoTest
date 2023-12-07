2c0287480a2876ade2ba4827906784c38127f3c9
Csege <cseloteigergely@gmail.com>
2023-12-07 11:23:58 +0100

    add error text locators to ReigsterPOM

--- src/main/java/logmyphone/autotest/Register/RegisterPOM.java
+++ src/main/java/logmyphone/autotest/Register/RegisterPOM.java
@@ -9,12 +9,20 @@ public class RegisterPOM {
     private WebDriver driver;
     @FindBy(id = "companyName")
     private WebElement nameField;
+    @FindBy(id = "companyName-helper-text")
+    private WebElement nameErrorText;
     @FindBy(id = "email")
     private WebElement emailField;
+    @FindBy(id = "email-helper-text")
+    private WebElement emailErrorText;
     @FindBy(id = "password")
     private WebElement passwordField;
+    @FindBy(id = "password-helper-text")
+    private WebElement passwordErrorText;
     @FindBy(id = "confirmPassword")
     private WebElement confirmPasswordField;
+    @FindBy(id = "confirmPassword-helper-text")
+    private WebElement confirmPasswordErrorText;
     @FindBy(xpath = "//button[className='css-1vhaqj4-MuiButtonBase-root-MuiButton-root'][text()='Register']")
     private WebElement registerButton;
     @FindBy(linkText = "Already have an account? Sign In")
