d1ec8cbf3cee94378589cf22a79decee20723885
Csege <cseloteigergely@gmail.com>
2023-12-07 13:45:16 +0100

    add beforeEach, beforeAll, afterAll and empty testcases to RegisterPomTest

--- src/test/java/logmyphone/autotest/Register/RegisterPOMTest.java
+++ src/test/java/logmyphone/autotest/Register/RegisterPOMTest.java
@@ -1,16 +1,44 @@
 package logmyphone.autotest.Register;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.jupiter.api.Assertions.*;
 class RegisterPOMTest {
+    private static WebDriver driver;
+    private RegisterPOM registerPOM;
 
+    @BeforeAll
+    static void beforeAll() {
+        driver = new ChromeDriver();
+    }
+    @AfterAll
+    static void afterAll() {
+        driver.close();
+    }
     @BeforeEach
-    void setUp() {
+    void beforeEach() {
+        registerPOM = new RegisterPOM(driver);
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    public void successfulRegistrationTest() {
     }
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/java/logmyphone/autotest/Register/resources/RegisterName.csv")
+    public void nameFieldTest() {}
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/java/logmyphone/autotest/Register/resources/RegisterEmail.csv")
+    public void emailFieldTest() {}
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/java/logmyphone/autotest/Register/resources/RegisterPassword.csv")
+    public void passwordFieldTest() {}
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/java/logmyphone/autotest/Register/resources/RegisterPassword.csv")
+    public void confirmPasswordFieldTest() {}
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/java/logmyphone/autotest/Register/resources/RegisterData.csv")
+    public void allFieldTest() {}
 }
